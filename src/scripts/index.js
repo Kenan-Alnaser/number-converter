5f497cd590a10a30f73acd9321ed29a89b07dcb2
Kenan Alnaser <kenan-na@live.com>
2021-05-04 15:04:24 +0200

    JS functionality added

---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+let userNumber = 0;
+const result = document.querySelector(".result");
+const userInput = document.querySelector("#userInput");
+const convertBtn = document.querySelector("#convertBtn");
+
+function registerUserValue(e) {
+  userNumber = e.target.value;
+}
+
+function convert() {
+  currentNumber = parseInt(userNumber);
+  return (result.innerHTML = currentNumber.toString(2));
+}
+
+userInput.addEventListener("keyup", registerUserValue);
+convertBtn.addEventListener("click", convert);
+e.preventDefault();
